f780470f70ecb4e1b4b2ad9d14e101f4209eef64
Hadi Raza <hadiraza.edu@gmail.com>
2023-10-13 17:13:19 +0500

    react-router

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+export default function Layout() {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
